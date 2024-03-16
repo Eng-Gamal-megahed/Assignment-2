db5973f82b8b610d3ea96d8c92210122cf7f217f
Eng.gamal <eng.gamalmegahed@gmail.com>
2024-03-16 02:57:26 +0200

    update regex and add multiplication and division V6

--- Fruction_calculator.cpp
+++ Fruction_calculator.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <regex>
+#include <limits>
 using namespace std;
 int findGCF(int num1 , int num2)
 {
@@ -18,123 +19,164 @@ int findGCF(int num1 , int num2)
 }
 
 int main ()
-{
-    regex rational("-?(([1-9]+[0-9]*)|0)(/[1-9]+[0-9]*)?");
-    int intnum1_prt1 , intnum1_prt2 , intnum2_prt1 , intnum2_prt2 , GCF , result_prt1 , result_prt2 , whole;
-    string num1 , num1_prt1 , num1_prt2 , num2 , num2_ptr1 , num2_prt2;
-    char chose;
-    bool valid , isminus_frst , isminus_scnd , foundslash = false;
-    cout << "Enter the first number :";
-    cin >> num1;
-    valid = regex_match(num1 , rational);
-    while(!valid){
-        cout << "Invalid number please follow the instructions\nEnter the first number :";
+{while (true){
+        regex rational("-?(([1-9]+\\.?[0-9]*)|0)(/[1-9]+\\.?[0-9]*)?");
+        int intnum1_prt1 , intnum1_prt2 , intnum2_prt1 , intnum2_prt2 , GCF , result_prt1 , result_prt2 , whole;
+        string num1 , num1_prt1 , num1_prt2 , num2 , num2_ptr1 , num2_prt2;
+        char chose;
+        bool valid , isminus_frst , isminus_scnd , foundslash = false;
+        cout << "Enter the first number :";
         cin >> num1;
         valid = regex_match(num1 , rational);
-    }
-    cout << "Enter the first number :";
-    cin >> num2;
-    valid = regex_match(num2 , rational);
-    while(!valid){
-        cout << "Invalid number please follow the instructions\nEnter the first number :";
+        while(!valid){
+            cout << "Invalid number please follow the instructions\nEnter the first number :";
+            cin >> num1;
+            valid = regex_match(num1 , rational);
+        }
+        cout << "Enter the second number :";
         cin >> num2;
         valid = regex_match(num2 , rational);
-    }
-    if(num1[0] == '-')
-    {
-        isminus_frst = true;
-        num1.erase(0,1);
-    }
-    if(num2[0] == '-')
-    {
-        isminus_scnd = true;
-        num2.erase(0,1);
-    }
-    for (char c : num1) {
-        if (c != '/' && !foundslash)
+        while(!valid){
+            cout << "Invalid number please follow the instructions\nEnter the second number :";
+            cin >> num2;
+            valid = regex_match(num2 , rational);
+        }
+        if(num1[0] == '-')
         {
-            continue;
+            isminus_frst = true;
+            num1.erase(0,1);
         }
-        else
-            foundslash = true;
-        if(foundslash)
+        if(num2[0] == '-')
         {
-            num1_prt2 += c;
+            isminus_scnd = true;
+            num2.erase(0,1);
         }
-    }
-    num1_prt2.erase(0,1);
-    intnum1_prt2 = stoi(num1_prt2);
-    foundslash = false;
-    for (char c : num2) {
-        if (c != '/' && !foundslash)
-        {
-            continue;
+        for (char c : num1) {
+            if (c != '/' && !foundslash)
+            {
+                continue;
+            }
+            else
+                foundslash = true;
+            if(foundslash)
+            {
+                num1_prt2 += c;
+            }
         }
-        else
-            foundslash = true;
-        if(foundslash)
-        {
-            num2_prt2 += c;
+        num1_prt2.erase(0,1);
+        intnum1_prt2 = stoi(num1_prt2);
+        foundslash = false;
+        for (char c : num2) {
+            if (c != '/' && !foundslash)
+            {
+                continue;
+            }
+            else
+                foundslash = true;
+            if(foundslash)
+            {
+                num2_prt2 += c;
+            }
         }
-    }
-    num2_prt2.erase(0,1);
-    intnum2_prt2 = stoi(num2_prt2);
-    foundslash = false;
-    for (char c : num1) {
-        if (c == '/') {
-            foundslash = true;
-            break;
+        num2_prt2.erase(0,1);
+        intnum2_prt2 = stoi(num2_prt2);
+        foundslash = false;
+        for (char c : num1) {
+            if (c == '/') {
+                foundslash = true;
+                break;
+            }
+            if (!foundslash) {
+                num1_prt1 += c;
+            }
         }
-        if (!foundslash) {
-            num1_prt1 += c;
+        intnum1_prt1 = stoi(num1_prt1);
+        (isminus_frst)? intnum1_prt1*=-1 : intnum1_prt2*=1 ;
+        foundslash =false;
+        for (char c : num2) {
+            if (c == '/') {
+                foundslash = true;
+                break;
+            }
+            if (!foundslash) {
+                num2_ptr1 += c;
+            }
         }
-    }
-    intnum1_prt1 = stoi(num1_prt1);
-    (isminus_frst)? intnum1_prt1*=-1 : intnum1_prt2*=1 ;
-    foundslash =false;
-    for (char c : num2) {
-        if (c == '/') {
-            foundslash = true;
-            break;
-        }
-        if (!foundslash) {
-            num2_ptr1 += c;
-        }
-    }
-    intnum2_prt1 = stoi(num2_ptr1);
-    (isminus_scnd)? intnum2_prt1*=-1 : intnum2_prt1*=1 ;
+        intnum2_prt1 = stoi(num2_ptr1);
+        (isminus_scnd)? intnum2_prt1*=-1 : intnum2_prt1*=1 ;
 
-    cout << "1)Addition\n2)Subtraction\n";
-    cin >> chose;
+        cout << "Chose an operation\n1)Addition\n2)Subtraction\n3)Multiplication\n4)Division\nEnter choice: ";
+        cin >> chose;
 
-    if (chose == '1')
-    {
-        result_prt1 = ((intnum1_prt1 * intnum2_prt2) + (intnum2_prt1 * intnum1_prt2));
-        result_prt2 = (intnum1_prt2 * intnum2_prt2);
-        whole = result_prt1 / result_prt2;
-        GCF = findGCF(abs(result_prt1),abs(result_prt2));
-        result_prt1 /= GCF;
-        result_prt2 /= GCF;
-        result_prt1 %= result_prt2;
-        if(whole){cout << whole << " ";}
-        if(result_prt1 != 0)
+        if (chose == '1')
         {
-            cout << result_prt1 << "/" << result_prt2;
+            result_prt1 = ((intnum1_prt1 * intnum2_prt2) + (intnum2_prt1 * intnum1_prt2));
+            result_prt2 = (intnum1_prt2 * intnum2_prt2);
+            whole = result_prt1 / result_prt2;
+            GCF = findGCF(abs(result_prt1),abs(result_prt2));
+            result_prt1 /= GCF;
+            result_prt2 /= GCF;
+            result_prt1 %= result_prt2;
+            if(whole){cout << whole << " "<<endl;}
+            if(result_prt1 != 0)
+            {
+                cout << result_prt1 << "/" << result_prt2<<endl;
+            }
+
         }
-    }
-    if (chose == '2')
-    {
-        result_prt1 = ((intnum1_prt1 * intnum2_prt2) - (intnum2_prt1 * intnum1_prt2));
-        result_prt2 = (intnum1_prt2 * intnum2_prt2);
-        whole = result_prt1 / result_prt2;
-        GCF = findGCF(abs(result_prt1),abs(result_prt2));
-        result_prt1 /= GCF;
-        result_prt2 /= GCF;
-        result_prt1 %= result_prt2;
-        if(whole){cout << whole << " ";}
-        if(result_prt1 != 0)
+        else if (chose == '2')
         {
-            cout << result_prt1 << "/" << result_prt2;
+            result_prt1 = ((intnum1_prt1 * intnum2_prt2) - (intnum2_prt1 * intnum1_prt2));
+            result_prt2 = (intnum1_prt2 * intnum2_prt2);
+            whole = result_prt1 / result_prt2;
+            GCF = findGCF(abs(result_prt1),abs(result_prt2));
+            result_prt1 /= GCF;
+            result_prt2 /= GCF;
+            result_prt1 %= result_prt2;
+            if(whole){cout << whole << " "<<endl;}
+            if(result_prt1 != 0)
+            {
+                cout << result_prt1 << "/" << result_prt2<<endl;
+            }
+        }
+        else if (chose=='3'){
+            result_prt1=(intnum1_prt1 * intnum2_prt1);
+            result_prt2=(intnum1_prt2 * intnum2_prt2);
+            whole = result_prt1 / result_prt2;
+            GCF = findGCF(abs(result_prt1),abs(result_prt2));
+            result_prt1 /= GCF;
+            result_prt2 /= GCF;
+            result_prt1 %= result_prt2;
+            if (whole){ cout << whole << " " << endl;}
+            else if (result_prt1 != 0){
+                cout<< result_prt1 << "/" << result_prt2 << endl;
+            }
+        }
+        else if (chose=='4'){
+            result_prt1=(intnum1_prt1 * intnum2_prt2);
+            result_prt2=(intnum1_prt2 * intnum2_prt1);
+            whole = result_prt1 / result_prt2;
+            GCF = findGCF(abs(result_prt1),abs(result_prt2));
+            result_prt1 /= GCF;
+            result_prt2 /= GCF;
+            result_prt1 %= result_prt2;
+            if (whole){cout << whole << " " <<endl;}
+            else if (result_prt1 != 0){
+                cout<< result_prt1 << "/" << result_prt2 << endl;
+            }
+        }
+        int choose;
+        cout<<"1) Another operation\n2) Exit\n ";
+        cin>>choose;
+        while (cin.fail() || (choose!=1 && choose!=2)){
+            cout<<"Invalid input! PLease, Follow the instructions."<<endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            cout<<"1) Another operation\n2) Exit\n ";
+            cin>>choose;
+        }
+        if (choose==2){
+            break;
         }
     }
     return 0 ;
